4dc40c4f2fe957dc7ec1869aa8693eb379445aab
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2023-11-26 13:13:28 -0800

    Add tailwind colors and change component file structure layout

--- src/stories/Page.stories.ts
+++ src/stories/Page.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { within, userEvent } from "@storybook/testing-library";
 
-import { Page } from "./Page";
+import { Page } from "../components/Page";
 
 const meta = {
   title: "Example/Page",
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#0079FF",
+        secondary: "#FFFFFF",
+        accent: "#6B21A8",
+      },
     },
   },
   plugins: [require("preline/plugin")],
