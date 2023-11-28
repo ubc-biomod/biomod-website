10eac1adec7fbc2767e691ce82cb4dcefa5c86f3
Daniel <57362494+daniel-panhead@users.noreply.github.com>
2023-11-27 17:31:55 -0800

    Daniel/make themes (#1)
    * Add tailwind colors and change component file structure layout
    
    * Configure storybook with tailwind/preline + make example component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -14,6 +15,17 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  webpackFinal: async (config: any) => {
+    // Add path aliases
+    config.resolve.alias["@"] = path.resolve(__dirname, "../src");
+    config.resolve.alias["@/app"] = path.resolve(__dirname, "../src/app");
+    config.resolve.alias["@/components"] = path.resolve(
+      __dirname,
+      "../src/components"
+    );
+
+    return config;
   },
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
 import type { Preview } from "@storybook/react";
+import "../src/app/globals.css";
+import("preline");
 
 const preview: Preview = {
   parameters: {
