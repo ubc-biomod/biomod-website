278088a794a9f7139e78aea299286038cc0fca01
Carson K <ckyba@students.cs.ubc.ca>
2024-04-01 02:14:32 -0700

    -fixed stuff that should be static

--- src/components/sponsors_tier.tsx
+++ src/components/sponsors_tier.tsx
@@ -12,81 +12,81 @@ type ContentItem = {
   link: string;
 };
 
+const goldContentData: ContentItem[] = [
+  {
+    image: "/vercel.svg",
+    header: "Gold Sponsor 1",
+    subheader: "Gold Subheader 1",
+    text: "Gold sponsor description 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi diam tortor, condimentum et mauris vitae, dapibus pretium justo. Suspendisse velit ex, luctus ac quam at, feugiat laoreet quam. Aenean a tortor tristique, commodo quam a, volutpat nisi. Morbi fermentum eu magna ut luctus. Duis ac sollicitudin lectus. Ut sodales convallis tempus. Aliquam et purus lacinia, pulvinar eros a, bibendum justo.",
+    link: "https://google.com",
+  },
+  {
+    image: "./gold_img1.jpg",
+    header: "Gold Sponsor 2",
+    subheader: "Gold Subheader 2",
+    text: "Gold sponsor description 2",
+    link: "https://google.com",
+  },
+  {
+    image: "./gold_img1.jpg",
+    header: "Gold Sponsor 3",
+    subheader: "Gold Subheader 3",
+    text: "Gold sponsor description 3",
+    link: "https://google.com",
+  },
+];
+
+const silverContentData: ContentItem[] = [
+  {
+    image: "./silver_img1.jpg",
+    header: "Silver Sponsor 1",
+    subheader: "Silver Subheader 1",
+    text: "Silver sponsor description 1",
+    link: "https://facebook.com",
+  },
+  {
+    image: "./silver_img1.jpg",
+    header: "Silver Sponsor 2",
+    subheader: "Silver Subheader 2",
+    text: "Silver sponsor description 2",
+    link: "https://facebook.com",
+  },
+  {
+    image: "./silver_img1.jpg",
+    header: "Silver Sponsor 3",
+    subheader: "Silver Subheader 3",
+    text: "Silver sponsor description 3",
+    link: "https://facebook.com",
+  },
+];
+
+const bronzeContentData: ContentItem[] = [
+  {
+    image: "./bronze_img1.jpg",
+    header: "Bronze Sponsor 1",
+    subheader: "Bronze Subheader 1",
+    text: "Bronze sponsor description 1",
+    link: "https://yahoo.com",
+  },
+  {
+    image: "./bronze_img1.jpg",
+    header: "Bronze Sponsor 2",
+    subheader: "Bronze Subheader 2",
+    text: "Bronze sponsor description 2",
+    link: "https://yahoo.com",
+  },
+  {
+    image: "./bronze_img1.jpg",
+    header: "Bronze Sponsor 3",
+    subheader: "Bronze Subheader 3",
+    text: "Bronze sponsor description 3",
+    link: "https://yahoo.com",
+  },
+];
+
 export const SponsorsTier: React.FC = () => {
   const [currentContent, setCurrentContent] = useState<number>(0);
   const [isTransitioning, setIsTransitioning] = useState<boolean>(false);
-
-  const goldContentData: ContentItem[] = [
-    {
-      image: "/vercel.svg",
-      header: "Gold Sponsor 1",
-      subheader: "Gold Subheader 1",
-      text: "Gold sponsor description 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi diam tortor, condimentum et mauris vitae, dapibus pretium justo. Suspendisse velit ex, luctus ac quam at, feugiat laoreet quam. Aenean a tortor tristique, commodo quam a, volutpat nisi. Morbi fermentum eu magna ut luctus. Duis ac sollicitudin lectus. Ut sodales convallis tempus. Aliquam et purus lacinia, pulvinar eros a, bibendum justo.",
-      link: "https://google.com",
-    },
-    {
-      image: "./gold_img1.jpg",
-      header: "Gold Sponsor 2",
-      subheader: "Gold Subheader 2",
-      text: "Gold sponsor description 2",
-      link: "https://google.com",
-    },
-    {
-      image: "./gold_img1.jpg",
-      header: "Gold Sponsor 3",
-      subheader: "Gold Subheader 3",
-      text: "Gold sponsor description 3",
-      link: "https://google.com",
-    },
-  ];
-
-  const silverContentData: ContentItem[] = [
-    {
-      image: "./silver_img1.jpg",
-      header: "Silver Sponsor 1",
-      subheader: "Silver Subheader 1",
-      text: "Silver sponsor description 1",
-      link: "https://facebook.com",
-    },
-    {
-      image: "./silver_img1.jpg",
-      header: "Silver Sponsor 2",
-      subheader: "Silver Subheader 2",
-      text: "Silver sponsor description 2",
-      link: "https://facebook.com",
-    },
-    {
-      image: "./silver_img1.jpg",
-      header: "Silver Sponsor 3",
-      subheader: "Silver Subheader 3",
-      text: "Silver sponsor description 3",
-      link: "https://facebook.com",
-    },
-  ];
-
-  const bronzeContentData: ContentItem[] = [
-    {
-      image: "./bronze_img1.jpg",
-      header: "Bronze Sponsor 1",
-      subheader: "Bronze Subheader 1",
-      text: "Bronze sponsor description 1",
-      link: "https://yahoo.com",
-    },
-    {
-      image: "./bronze_img1.jpg",
-      header: "Bronze Sponsor 2",
-      subheader: "Bronze Subheader 2",
-      text: "Bronze sponsor description 2",
-      link: "https://yahoo.com",
-    },
-    {
-      image: "./bronze_img1.jpg",
-      header: "Bronze Sponsor 3",
-      subheader: "Bronze Subheader 3",
-      text: "Bronze sponsor description 3",
-      link: "https://yahoo.com",
-    },
-  ];
 
   const [currentContentData, setCurrentContentData] =
     useState<ContentItem[]>(goldContentData);
@@ -141,27 +141,25 @@ export const SponsorsTier: React.FC = () => {
     currentContentData[currentContent];
 
   return (
-    <div
-      className={`flex sm: flex-col sm:flex-row bg-white border rounded-xl shadow-sm dark:bg-gray-300 dark:border-gray-700 dark:grey/[.7]`}
-    >
+    <div className="flex flex-col sm:flex-row rounded-xl shadow-sm bg-gray-300 border-gray-700 h-[750px] md:h-[500px]">
       {/* DNA Pic */}
-      <div className="flex flex-row flex-shrink-0 w-full rounded-t-xl overflow-hidden sm:rounded-s-xl sm:w-1/4 md:rounded-se-none md:max-w-2/5 h-20 md:h-auto bg-emerald-500 content-center">
-        <div className="flex md:flex-col flex-row m-auto md:w-2/5 md:h-4/5 w-4/5 h-4/5 bg-red-500 content-center justify-around">
+      <div className="flex flex-row flex-shrink-0 w-full rounded-t-xl sm:rounded-s-xl sm:w-1/4 md:rounded-se-none h-auto bg-emerald-500 items-center">
+        <div className="flex md:flex-col flex-row m-auto md:w-28 md:h-80 w-56 h-16 my-3 bg-red-500 content-center justify-around">
           {/* Navigation buttons for different sponsor types */}
           <button
-            className="content-button text-white"
+            className="text-white"
             onClick={() => handleContentChange("gold")}
           >
             Gold Sponsors
           </button>
           <button
-            className="content-button text-white"
+            className="text-white"
             onClick={() => handleContentChange("silver")}
           >
             Silver Sponsors
           </button>
           <button
-            className="content-button text-white"
+            className="text-white"
             onClick={() => handleContentChange("bronze")}
           >
             Bronze Sponsors
@@ -170,23 +168,23 @@ export const SponsorsTier: React.FC = () => {
       </div>
 
       {/* Right Content */}
-      <div className={`flex flex-wrap w-full sm:px-5`}>
-        <div
-          className={`p-4 flex flex-col h-full w-full sm:p-7  ${
-            transitionDirection === "left" ? "cardTransitionLeft" : ""
-          }
-    ${transitionDirection === "right" ? "cardTransitionRight" : ""}
-    ${transitionDirection === "newCard" ? "newCardTransition" : ""}`}
-          onAnimationEnd={handleTransitionEnd}
-        >
-          <div className="flex flex-row items-center justify-between">
+      <div className="flex w-full sm:px-5 items-center  my-auto">
+        <div className="p-4 flex flex-col h-full w-full sm:p-7">
+          <div className="flex flex-row items-center justify-between h-[600px] sm:h-[500px] md:h-[350px]">
             <button
               className="nav-button sm:mx-10 mr-3"
               onClick={() => handleNavigation("prev")}
             >
               &lt;
             </button>
-            <div className="flex flex-col">
+            <div
+              className={`flex flex-col ${
+                transitionDirection === "left" ? "cardTransitionLeft" : ""
+              }
+    ${transitionDirection === "right" ? "cardTransitionRight" : ""}
+    ${transitionDirection === "newCard" ? "newCardTransition" : ""}`}
+              onAnimationEnd={handleTransitionEnd}
+            >
               <div className="flex flex-row items-center">
                 <div className="logo-temp"></div>
                 <h2 className="text-2xl font-bold black">{header}</h2>
