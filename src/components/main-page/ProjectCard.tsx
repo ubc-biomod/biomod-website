4cf8803b25f02f4cc74b68cf18c933985f19e46c
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-05-25 13:38:15 -0700

    Add year transition hover animation with framer motion

--- src/components/main-page/ProjectCard.tsx
+++ src/components/main-page/ProjectCard.tsx
@@ -1,30 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 type ProjectCardProps = {
   className?: string;
   children: React.ReactNode | React.ReactNode[];
   yearElement: React.ReactNode | React.ReactNode[];
-  startActivated?: boolean;
+  startOverlayActivated?: boolean;
 };
 
 const ProjectCard = ({
   className = "",
   children,
-  yearElement: year,
-  startActivated = true,
+  yearElement,
+  startOverlayActivated = false,
 }: ProjectCardProps) => {
+  const [yearOverlayActive, setYearOverlayActive] = useState(
+    startOverlayActivated
+  );
+
+  const handleKeyPress: React.KeyboardEventHandler<HTMLDivElement> = (e) => {
+    if (e.key === " ") {
+      e.preventDefault();
+      setYearOverlayActive((prev) => !prev);
+    }
+  };
+
   return (
     <div
-      className={`bg-secondary h-full flex justify-center items-center rounded-lg lg:rounded-xl ${className}`}
+      className={`bg-primary relative h-full flex justify-center items-center rounded-lg lg:rounded-xl ${className}`}
+      onMouseEnter={() => setYearOverlayActive(true)}
+      onMouseLeave={() => setYearOverlayActive(false)}
+      onKeyDown={handleKeyPress}
+      tabIndex={0}
     >
-      {!startActivated && children}
-      {startActivated && (
-        <div>
-          <div className="text-3xl lg:text-6xl font-semibold text-primary">
-            {year}
-          </div>
-        </div>
-      )}
+      <AnimatePresence>
+        {yearOverlayActive && (
+          <motion.div
+            className="absolute flex justify-center items-center w-full h-full rounded-lg lg:rounded-xl bg-secondary text-3xl lg:text-6xl font-semibold text-primary"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.1 }}
+          >
+            {yearElement}
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <div className="">{children}</div>
+      {/* {!yearOverlayActive && 
+        children
+      }
+      {yearOverlayActive && (
+        
+      )} */}
     </div>
   );
 };
