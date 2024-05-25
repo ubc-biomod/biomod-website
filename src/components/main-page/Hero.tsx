533c4a7cd8041c822c3a2c82aeec907c59d9c17b
Minh Au <auduongminh2004@gmail.com>
2024-05-25 02:00:53 -0400

    fix some minor responsive error

--- src/components/main-page/Hero.tsx
+++ src/components/main-page/Hero.tsx
@@ -2,6 +2,7 @@ import CellImage from "@/assets/cell.png";
 import Image from "next/image";
 import { AnimatePresence, motion } from "framer-motion";
 import { useRef, useState } from "react";
+import Link from "next/link";
 
 export default function Hero() {
   return (
@@ -9,36 +10,37 @@ export default function Hero() {
     <div>
       <div
         className="flex flex-row flex-wrap items-center justify-center
-                lg:flex-nowrap "
+                xl:flex-nowrap "
       >
         <Image
-          className="lg:w-[50rem] lg:h-[50rem]"
+          className="xl:w-[50rem] xl:h-[50rem]"
           src={CellImage}
           alt="Hand-drawn Image of Cell"
         />
         <div
           className="flex flex-col w-fit items-center gap-y-[1.0rem]
-                    lg:items-start lg:gap-y-[2.25rem]"
+                    xl:items-start xl:gap-y-[2.25rem]"
         >
           <h1
             className="text-primary text-[3.5rem] text-center
-                        md:text-[6rem] lg:text-left"
+                        md:text-[6rem] xl:text-left"
           >
             UBC BIOMOD
           </h1>
           <h2
             className="text-center text-subHeadingRegMob
-                        md:text-subHeadingReg lg:text-left"
+                        md:text-subHeadingReg xl:text-left"
           >
             This is gonna say something so super duper cool about biomod that
             everyone will freak and scroll through the entire website!
           </h2>
-          <div
+          <Link
+            href={"/about"}
             className="bg-accent grow-0 px-[2.5rem] py-[1rem] rounded-[0.75rem]
                         mt-[1.4rem] text-white cursor-pointer hover:opacity-80"
           >
             Discover
-          </div>
+          </Link>
         </div>
       </div>
 
