7eb9ac81facd1573a668cd5248dd67e9bf7b1695
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-06-08 10:42:26 -0700

    Populate with real logos

--- src/components/main-page/Sponsors.tsx
+++ src/components/main-page/Sponsors.tsx
@@ -1,23 +1,58 @@
+import Image from "next/image";
 import React from "react";
+import ecoscope from "../../assets/sponsors/ecoscope.png";
+import google from "../../assets/sponsors/google.png";
+import chbe from "../../assets/sponsors/chbe.png";
+
+const sponsorsList = [
+  {
+    href: "#",
+    component: <Image src={ecoscope} alt="Ecoscope Logo" key="Ecoscope" />,
+  },
+  {
+    href: "#",
+    component: <Image src={google} alt="Google Logo" key="Google" />,
+  },
+  {
+    href: "#",
+    component: <Image src={chbe} alt="CHBE Logo" key="CHBE" />,
+  },
+  {
+    href: "#",
+    component: <Image src={ecoscope} alt="Ecoscope Logo" key="Ecoscope" />,
+  },
+  {
+    href: "#",
+    component: <Image src={google} alt="Google Logo" key="Google" />,
+  },
+  {
+    href: "#",
+    component: <Image src={chbe} alt="CHBE Logo" key="CHBE" />,
+  },
+];
 
 const Sponsors = () => {
   const steps = [0.25, 0.5, 1, 0.5, 0.25];
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col items-center gap-8 overflow-hidden">
       {steps.map((opacity, i) => {
-        return <FakeSponsor key={i} style={{ opacity: opacity }} />;
+        return (
+          <div
+            className="flex gap-4 items-center justify-between"
+            key={opacity + i + ""}
+            style={{ opacity: opacity }}
+          >
+            {sponsorsList.map((sponsor, k) => (
+              <a href={sponsor.href} target="_blank" key={sponsor.href + k}>
+                {sponsor.component}
+              </a>
+            ))}
+          </div>
+        );
       })}
     </div>
   );
 };
-
-const FakeSponsor = ({ style }: { style?: React.CSSProperties }) => (
-  <div className="flex gap-4 overflow-hidden">
-    {Array.from(Array(7)).map((_, i) => (
-      <div className="w-40 h-32 bg-primary" style={style} key={i}></div>
-    ))}
-  </div>
-);
 
 export default Sponsors;
