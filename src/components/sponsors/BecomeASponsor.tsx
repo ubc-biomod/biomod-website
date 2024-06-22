c2172eb0d51b9d02c95b237136b94aac727ea2ab
piano <piano.trumpet.cello@gmail.com>
2024-06-22 11:23:01 -0700

    Fixed merge for BecomeASponsor

---
+++ src/components/sponsors/BecomeASponsor.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const BecomeASponsor: React.FC = () => {
+  return (
+    <>
+      <div className="text-lg">Become A Sponsor
+        <button><link href="/SponsorForm.tsx"> Become a sponsor</link></button>
+      </div>
+      
+    </>
+  );
+};
+
+export default BecomeASponsor;
---
+++ src/components/sponsors/SponsorForm.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const BecomeASponsor: React.FC = () => {
+  return (
+    <>
+      <p> Thank you for donating to our cause! </p>
+      <button><link rel="Return to Sponsor Page" href="/BecomeASponsor.tsx" /></button>
+      
+    </>
+  );
+};
+
+export default BecomeASponsor;
