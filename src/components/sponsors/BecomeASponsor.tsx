03468f65970a23c5b5e143505eb7ae09cab8acc2
piano <piano.trumpet.cello@gmail.com>
2024-06-29 11:13:58 -0700

    Finished become a sponsor

--- src/components/sponsors/BecomeASponsor.tsx
+++ src/components/sponsors/BecomeASponsor.tsx
@@ -4,7 +4,8 @@ import React from "react";
 const BecomeASponsor: React.FC = () => {
   return (
     <>
-      <div className="text-lg">Become A Sponsor
+      <div className="text-lg">
+        <p> Want to sypport our cause? Become a sponsor!</p>
         <button><link href="/SponsorForm.tsx"> Become a sponsor</link></button>
       </div>
       
