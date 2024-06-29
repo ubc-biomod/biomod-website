c0970c835672968fb089cd94fa777b56209fcfaf
piano <piano.trumpet.cello@gmail.com>
2024-06-29 11:13:58 -0700

    Finished become a sponsor

--- src/components/sponsors/SponsorForm.tsx
+++ src/components/sponsors/SponsorForm.tsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
+
+import ReactDOM from 'react-dom/client';
 
 
-const BecomeASponsor: React.FC = () => {
+const SponsorForm: React.FC = () => {
   return (
+
+    
     <>
+      <form >
+        <label>Please enter your company's name:
+          <input type="text" />
+        </label>
+
+        <label>Please upload a png of your company's logo:
+            <input type="file" />
+        </label>
+
+        <input type="submit" />
+      </form>
+
       <p> Thank you for donating to our cause! </p>
-      <button><link rel="Return to Sponsor Page" href="/BecomeASponsor.tsx" /></button>
+      <button><link rel="Return to Sponsor Page" href="/BecomeASponsor.tsx"/></button>
       
     </>
   );
 };
 
-export default BecomeASponsor;
+export default SponsorForm;
