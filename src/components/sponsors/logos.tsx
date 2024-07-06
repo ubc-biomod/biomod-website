c4c298642ade33c2d72ac1881ea2f0235742d772
piano <piano.trumpet.cello@gmail.com>
2024-07-06 12:06:21 -0700

    Become a sponsor storybook component added

---
+++ src/components/sponsors/logos.tsx
@@ -0,0 +1,25 @@
+const logos = [
+    {
+    name: 'ecoscope',
+    logo: '',
+    id:1
+
+  },
+ {
+    name: 'IDT DNA',
+    logo: '',
+    id:2
+   },
+   {
+    name: 'Starfish Medical',
+    logo: '',
+    id:3
+
+  },
+  {
+    name: 'Juno Therapeudics',
+    logo: '',
+    id:4
+
+  },
+ ];
