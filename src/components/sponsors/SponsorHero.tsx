bbffaa42767e6db10d7edf64db9bdf03fd01b869
piano <piano.trumpet.cello@gmail.com>
2024-07-06 12:00:15 -0700

    Added Storybook component for "Become a sponsor"

--- src/components/sponsors/SponsorHero.tsx
+++ src/components/sponsors/SponsorHero.tsx
@@ -1,3 +1,35 @@
 import React from "react";
 
 
+const seeLogos = () => {
+  const animals = [
+    { id: 1, animal: "Dog" },
+    { id: 2, animal: "Bird" },
+    { id: 3, animal: "Cat" },
+    { id: 4, animal: "Mouse" },
+    { id: 5, animal: "Horse" }
+  ];
+    return (
+      <>
+        <ul>
+          <map name="logos"></map>
+        </ul>
+        
+      </>
+    );
+  };
+
+  const dragDrop: React.FC = (prop) => {
+    return (
+      <>
+        for(){
+
+        }
+        
+      </>
+    );
+
+  };
+  
+
+  
