20b2b08477823cfb48849f12c5df2d9377822eb9
piano <piano.trumpet.cello@gmail.com>
2024-07-06 12:12:01 -0700

    Edited main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,31 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
+    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
-  staticDirs: ["..\\public"],
+  docs: {
+    autodocs: "tag",
+  },
+  webpackFinal: async (config: any) => {
+    // Add path aliases
+    config.resolve.alias["@"] = path.resolve(__dirname, "../src");
+    config.resolve.alias["@/app"] = path.resolve(__dirname, "../src/app");
+    config.resolve.alias["@/components"] = path.resolve(
+      __dirname,
+      "../src/components"
+    );
+
+    return config;
+  },
 };
 export default config;
