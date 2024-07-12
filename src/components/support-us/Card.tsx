d4099b338c7b3fcf403ed9ed7f7e97dbd3c60361
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-07-12 01:13:08 -0700

    Lay out HelpOurTeam with grid for equal card size

--- src/components/support-us/Card.tsx
+++ src/components/support-us/Card.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
 import React from "react";
 
 type CardProps = {
+  className?: string;
   title: string;
   subtitle: string;
   body: string;
@@ -9,20 +9,27 @@ type CardProps = {
   img?: string;
 };
 
-function Card({ title, subtitle, body, href, img }: CardProps) {
+function Card({ className = "", title, subtitle, body, href, img }: CardProps) {
   return (
-    <div className="flex flex-col bg-[#F9F9F9] border shadow-sm rounded-xl">
+    <div
+      className={
+        "flex flex-col bg-[#F9F9F9] border shadow-sm rounded-xl " + className
+      }
+    >
       {img && (
+        // eslint-disable-next-line @next/next/no-img-element
         <img
           className="w-full h-auto rounded-t-xl"
           src={img}
           alt="Image Description"
         />
       )}
-      <div className="p-4 md:p-5">
-        <h3 className="text-2xl font-bold text-primary">{title}</h3>
-        <p className="mt-1 text-sm text-gray-500 font-semibold">{subtitle}</p>
-        <p className="mt-6 text-gray-500">{body}</p>
+      <div className="flex flex-col justify-between p-4 md:p-5 grow">
+        <div>
+          <h3 className="text-2xl font-bold text-primary">{title}</h3>
+          <p className="mt-1 text-sm text-gray-500 font-semibold">{subtitle}</p>
+          <p className="mt-6 text-gray-500">{body}</p>
+        </div>
         <a
           href={href}
           className="w-fit mt-5 flex items-center gap-2 text-primary font-semibold hover:opacity-80 transition-opacity duration-150"
--- src/components/support-us/HelpOurTeam.tsx
+++ src/components/support-us/HelpOurTeam.tsx
@@ -7,28 +7,27 @@ function HelpOurTeam() {
       <h2 className="text-3xl md:text-5xl text-center font-bold text-primary">
         Ways you could help our team
       </h2>
-      <div className="flex flex-col gap-4 md:flex-row w-full">
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 grid-rows-3 md:grid-rows-2 w-full">
         <Card
+          className="md:row-span-2"
           title="Join our team and make an impact with us"
           subtitle="Subtitle"
           body="Some quick example text to build on the card title and make up the bulk of the card's content."
           img="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80"
           href="#"
         />
-        <div className="flex flex-col gap-4 md:w-1/2 shrink-0 h-full">
-          <Card
-            title="Donate as an industry"
-            subtitle="Something something that identifies as an industry"
-            body="Some quick example text to build on the card title and make up the bulk of the card's content."
-            href="#"
-          />
-          <Card
-            title="Donate as an individual"
-            subtitle="Something something that identifies as an industry"
-            body="Some quick example text to build on the card title and make up the bulk of the card's content."
-            href="#"
-          />
-        </div>
+        <Card
+          title="Donate as an industry"
+          subtitle="Something something that identifies as an industry"
+          body="Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content."
+          href="#"
+        />
+        <Card
+          title="Donate as an individual"
+          subtitle="Something something that identifies as an industry"
+          body="Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content."
+          href="#"
+        />
       </div>
     </div>
   );
