5d400dfb3ffb7c90be705466f1d982aa1cb723c2
Carson K <ckyba@students.cs.ubc.ca>
2024-07-15 23:20:30 -0700

    -re-implemented with framer motion -no stutters (at least on my device) -if logo sizes need to be changed, im pretty sure the size change needs to be proportional to the x value of animate. Kinda confusing but doing so ensures no stutters

--- src/components/sponsors/PastSponsors.tsx
+++ src/components/sponsors/PastSponsors.tsx
@@ -1,160 +1,87 @@
+import { motion } from "framer-motion";
 import amazonIcon from "@/assets/sponsors/amazon-text.png";
 import googleIcon from "@/assets/sponsors/google-text.png";
 import drracketIcon from "@/assets/sponsors/drracket-text.png";
 import microsoftIcon from "@/assets/sponsors/microsoft-text.png";
-
-type StaticImageData = {
-  src: string;
-};
+import Image from "next/image";
+import { useMediaQuery } from "react-responsive";
 
 export const PastSponsors: React.FC = () => {
+  const slides = [
+    {
+      href: "https://www.amazon.com",
+      src: amazonIcon,
+      alt: "Amazon Logo Wide",
+    },
+    {
+      href: "https://www.google.com",
+      src: googleIcon,
+      alt: "Google Logo Wide",
+    },
+    {
+      href: "https://racket-lang.org/",
+      src: drracketIcon,
+      alt: "DrRacket Logo Wide",
+    },
+    {
+      href: "https://www.microsoft.com",
+      src: microsoftIcon,
+      alt: "Microsoft Logo Wide",
+    },
+  ];
+  const duplicatedSlides = [...slides, ...slides];
+
+  const isDesktop = useMediaQuery({ query: "(min-width: 768px)" });
+
   return (
-    <div className="flex flex-col md:w-3/4 mx-auto">
-      <h2 className="text-5xl md:text-7xl my-10 text-center text-primary">
+    <div className="flex flex-col w-full h-full">
+      <div className="text-5xl md:text-7xl mx-auto text-center text-primary z-10 mb-6 md:mb-8">
         Past Sponsors
-      </h2>
-      <div className="flex overflow-hidden">
-        <div className="flex space-x-2 ml-2 md:ml-8 md:space-x-8 animate-loop-scroll">
-          <a
-            href="https://www.amazon.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={(amazonIcon as StaticImageData).src}
-              className="h-12 md:h-14 max-w-none filter grayscale hover:grayscale-0"
-              alt="Amazon Logo Wide"
-            />
-          </a>
-          <a
-            href="https://www.google.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={(googleIcon as StaticImageData).src}
-              className="h-12 md:h-14 max-w-none mt-6 md:mt-12 filter grayscale hover:grayscale-0"
-              alt="Google Logo Wide"
-            />
-          </a>
-          <a
-            href="https://racket-lang.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={(drracketIcon as StaticImageData).src}
-              className="h-12 md:h-14 max-w-none filter grayscale hover:grayscale-0"
-              alt="DrRacket Logo Wide"
-            />
-          </a>
-          <a
-            href="https://www.microsoft.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={(microsoftIcon as StaticImageData).src}
-              className="h-12 md:h-14 max-w-none mt-6 md:mt-12 filter grayscale hover:grayscale-0"
-              alt="Microsoft Logo Wide"
-            />
-          </a>
-        </div>
-        <div className="flex space-x-2 ml-2 md:ml-8 md:space-x-8 animate-loop-scroll">
-          <a
-            href="https://www.amazon.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={(amazonIcon as StaticImageData).src}
-              className="h-12 md:h-14 max-w-none filter grayscale hover:grayscale-0"
-              alt="Amazon Logo Wide"
-            />
-          </a>
-          <a
-            href="https://www.google.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={(googleIcon as StaticImageData).src}
-              className="h-12 md:h-14 max-w-none mt-6 md:mt-12 filter grayscale hover:grayscale-0"
-              alt="Google Logo Wide"
-            />
-          </a>
-          <a
-            href="https://racket-lang.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={(drracketIcon as StaticImageData).src}
-              className="h-12 md:h-14 max-w-none filter grayscale hover:grayscale-0"
-              alt="DrRacket Logo Wide"
-            />
-          </a>
-          <a
-            href="https://www.microsoft.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={(microsoftIcon as StaticImageData).src}
-              className="h-12 md:h-14 max-w-none mt-6 md:mt-12 filter grayscale hover:grayscale-0"
-              alt="Microsoft Logo Wide"
-            />
-          </a>
-        </div>
-        <div
-          className="flex space-x-2 ml-2 md:ml-8 md:space-x-8 animate-loop-scroll"
-          aria-hidden="true"
+      </div>
+      <div
+        className="relative h-full overflow-hidden pb-6 bg-white mx-auto z-0"
+        style={{ width: "80%" }}
+      >
+        <div className="absolute inset-0 z-10 pointer-events-none before:absolute before:left-0 before:top-0 before:w-1/4 before:h-full before:bg-gradient-to-r before:from-white before:to-transparent before:pointer-events-none after:absolute after:right-0 after:top-0 after:w-1/4 after:h-full after:bg-gradient-to-l after:from-white after:to-transparent after:pointer-events-none"></div>
+        <motion.div
+          className="flex"
+          animate={{
+            x: ["0%", isDesktop ? "-100%" : "-200%"], // if logos need to be different size, must be proportional to this I think
+            transition: {
+              ease: "linear",
+              duration: 15,
+              repeat: Infinity,
+            },
+          }}
+          style={{ zIndex: 20 }} // make sure button is clickable above gradient
         >
-          <a
-            href="https://www.amazon.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={(amazonIcon as StaticImageData).src}
-              className="h-12 md:h-14 max-w-none filter grayscale hover:grayscale-0"
-              alt="Amazon Logo Wide"
-            />
-          </a>
-          <a
-            href="https://www.google.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={(googleIcon as StaticImageData).src}
-              className="h-12 md:h-14 max-w-none mt-6 md:mt-12 filter grayscale hover:grayscale-0"
-              alt="Google Logo Wide"
-            />
-          </a>
-          <a
-            href="https://www.drracket.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={(drracketIcon as StaticImageData).src}
-              className="h-12 md:h-14 max-w-none filter grayscale hover:grayscale-0"
-              alt="DrRacket Logo Wide"
-            />
-          </a>
-          <a
-            href="https://www.microsoft.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={(microsoftIcon as StaticImageData).src}
-              className="h-12 md:h-14 max-w-none mt-6 md:mt-12 filter grayscale hover:grayscale-0"
-              alt="Microsoft Logo Wide"
-            />
-          </a>
-        </div>
+          {duplicatedSlides.map((slide, index) => (
+            <div
+              key={index}
+              className="flex-shrink-0"
+              style={{
+                width: `${
+                  isDesktop ? 100 / slides.length : (100 / slides.length) * 2
+                }%`,
+              }}
+            >
+              <div
+                className={`flex items-center justify-center h-full ml-8 ${
+                  index % 2 !== 0 ? "mt-6 md:mt-12" : ""
+                }`}
+              >
+                <a
+                  href={slide.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="grayscale hover:grayscale-0 transition duration-300"
+                >
+                  <Image src={slide.src} alt={slide.alt} />
+                </a>
+              </div>
+            </div>
+          ))}
+        </motion.div>
       </div>
     </div>
   );
