e3ecdb34e066b0e8ea5bb8b4d16768612c49d250
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-07-19 00:56:15 -0700

    Populate sponsor tier sponsors

--- src/components/sponsors/SponsorsTier.tsx
+++ src/components/sponsors/SponsorsTier.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import amazonIcon from "@/assets/sponsors/amazon-logo.png";
-import googleIcon from "@/assets/sponsors/google-logo.png";
-import drracketIcon from "@/assets/sponsors/drracket-logo.png";
-import microsoftIcon from "@/assets/sponsors/microsoft-logo.png";
-import teslaIcon from "@/assets/sponsors/tesla-logo.png";
+
+import sbme from "./sbme.png";
+import apsc from "./apsc.png";
+import msl from "./msl.png";
+import neBiolabs from "./ne_biolabs.jpg";
+import tilibit from "./tilibit.jpg";
+
 import { motion } from "framer-motion";
 import Image, { StaticImageData } from "next/image";
 
@@ -22,95 +24,57 @@ type ContentItem = {
   link: string;
 };
 
-const goldContentData: ContentItem[] = [
+const platContentData: ContentItem[] = [
   {
-    image: googleIcon,
-    header: "Gold Sponsor 1",
+    image: apsc,
+    header: "UBC Faculty of Applied Science",
     subheader: "Gold Subheader 1",
-    text: "Gold sponsor description 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi diam tortor, condimentum et mauris vitae, dapibus pretium justo. Suspendisse velit ex, luctus ac quam at, feugiat laoreet quam. Aenean a tortor tristique, commodo quam a, volutpat nisi. Morbi fermentum eu magna ut luctus. Duis ac sollicitudin lectus. Ut sodales convallis tempus. Aliquam et purus lacinia, pulvinar eros a, bibendum justo.",
-    link: "https://google.com",
+    text: "UBC Applied Science comprises a unique constellation of disciplines. Our core purpose is to discover, design, innovate, provide unwavering top-tier education and champion a community of responsible professionals.",
+    link: "https://apsc.ubc.ca/",
   },
   {
-    image: drracketIcon,
-    header: "Gold Sponsor 2",
+    image: sbme,
+    header: "UBC School of Biomedical Engineering",
     subheader: "Gold Subheader 2",
-    text: "Gold sponsor description 2",
-    link: "https://google.com",
-  },
-  {
-    image: amazonIcon,
-    header: "Gold Sponsor 3",
-    subheader: "Gold Subheader 3",
-    text: "Gold sponsor description 3",
-    link: "https://google.com",
-  },
-];
-
-const silverContentData: ContentItem[] = [
-  {
-    image: microsoftIcon,
-    header: "Silver Sponsor 1",
-    subheader: "Silver Subheader 1",
-    text: "Silver sponsor description 1",
-    link: "https://facebook.com",
-  },
-  {
-    image: teslaIcon,
-    header: "Silver Sponsor 2",
-    subheader: "Silver Subheader 2",
-    text: "Silver sponsor description 2",
-    link: "https://facebook.com",
-  },
-  {
-    image: amazonIcon,
-    header: "Silver Sponsor 3",
-    subheader: "Silver Subheader 3",
-    text: "Silver sponsor description 3",
-    link: "https://facebook.com",
+    text: "Created in 2017 through a partnership between the Faculties of Applied Science and Medicine, and enabled by the leadership of British Columbia's provincial government, the School of Biomedical Engineering (SBME) is Canada's living laboratory for new models of convergent research and education.",
+    link: "https://bme.ubc.ca/",
   },
 ];
 
 const bronzeContentData: ContentItem[] = [
   {
-    image: drracketIcon,
-    header: "Bronze Sponsor 1",
-    subheader: "Bronze Subheader 1",
-    text: "Bronze sponsor description 1",
-    link: "https://yahoo.com",
-  },
-  {
-    image: googleIcon,
-    header: "Bronze Sponsor 2",
+    image: msl,
+    header: "UBC Michael Smith Laboratories",
     subheader: "Bronze Subheader 2",
-    text: "Bronze sponsor description 2",
-    link: "https://yahoo.com",
+    text: "Envisioned by Dr. Michael Smith, a Nobel Laureate and a pioneer at the interface of biology and technology, UBC’s Biotechnology Laboratory (later renamed in 2004 when the new building opened as the Michael Smith Laboratories) was the first of its kind: a dynamic hub that includes clusters of interdisciplinary researchers working together, under one roof. Fast-forward two decades, the Michael Smith Laboratories (MSL) is now home to a team of researchers from a number of faculties including Science, Medicine, Applied Science, Forestry, and Land and Food Systems.",
+    link: "https://www.msl.ubc.ca/",
   },
   {
-    image: teslaIcon,
-    header: "Bronze Sponsor 3",
+    image: neBiolabs,
+    header: "New England Biolabs",
     subheader: "Bronze Subheader 3",
-    text: "Bronze sponsor description 3",
-    link: "https://yahoo.com",
+    text: 'Created "by scientists for scientists", NEB is renowned for consistently providing exceptional product quality and unsurpassed technical support. For over four decades, NEB has been shaping the landscape of bioscience research by discovering, developing and supporting superior research reagents. From our founding principles – placing the advancement of science and the stewardship of the environment as our highest priorities – to our unique corporate culture, NEB’s philosophy can be distilled down to three core values: passion, humility and being genuine.',
+    link: "https://www.neb.ca/about.php",
   },
   {
-    image: amazonIcon,
-    header: "Bronze Sponsor 4",
+    image: tilibit,
+    header: "Tilibit Nanosystems",
     subheader: "Bronze Subheader 4",
-    text: "Bronze sponsor description 4",
-    link: "https://yahoo.com",
+    text: "We believe that we can revolutionize nanoscale engineering through molecular information technology. We do this by programming DNA - the highest information density material in the world - to self-assemble into defined structures of arbitrary shape. This allows us to create devices of unprecedented complexity with nanometer accuracy. By engineering information rather than methods, we can deliver products of exceptional quality.",
+    link: "https://www.tilibit.com/",
   },
 ];
 
 const contentData: Record<Tier, ContentItem[]> = {
-  [Tier.PLAT]: [],
-  [Tier.GOLD]: goldContentData,
-  [Tier.SILVER]: silverContentData,
+  [Tier.PLAT]: platContentData,
+  [Tier.GOLD]: [],
+  [Tier.SILVER]: [],
   [Tier.BRONZE]: bronzeContentData,
 };
 
 function SponsorsTier() {
   const [sponsorIndex, setSponsorIndex] = useState({ curr: 0, prev: 0 });
-  const [tierIndex, setTierIndex] = useState({ curr: Tier.GOLD, prev: -1 });
+  const [tierIndex, setTierIndex] = useState({ curr: Tier.PLAT, prev: -1 });
 
   function onNavLeft() {
     setSponsorIndex((prev) => ({
@@ -184,15 +148,9 @@ function SponsorsTier() {
           {/* Navigation buttons for different sponsor types */}
           <button
             className="text-white"
-            onClick={() => handleContentChange(Tier.GOLD)}
+            onClick={() => handleContentChange(Tier.PLAT)}
           >
-            Gold Sponsors
-          </button>
-          <button
-            className="text-white"
-            onClick={() => handleContentChange(Tier.SILVER)}
-          >
-            Silver Sponsors
+            Platinum Sponsors
           </button>
           <button
             className="text-white"
@@ -260,7 +218,7 @@ function SponsorsTier() {
               <Image
                 src={image.src}
                 alt={header}
-                className="w-16 h-16 mr-4"
+                className="w-16 h-16 mr-4 shadow-md"
                 width={16}
                 height={16}
               />
@@ -315,7 +273,7 @@ function SponsorsTier() {
               <Image
                 src={content.image.src}
                 alt={content.header}
-                className="w-8 h-8"
+                className="w-8 h-8 shadow-sm"
                 layout="fill"
               />
             </button>
