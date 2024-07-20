87cf1dc2078a9c5f54ee464e8bb3d3f06643aac6
Minh Au <auduongminh2004@gmail.com>
2024-07-19 22:44:45 -0400

    add space between components

--- src/app/support-us/page.tsx
+++ src/app/support-us/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/support-us/Hero";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-y-[5rem]">
       <Hero />
       <Budget />
     </div>
