eb7acf755bd7b6fad9bf9040231a89b6dee8db25
Minh Au <auduongminh2004@gmail.com>
2024-07-19 23:44:50 -0400

    add phases of budget

--- src/app/support-us/page.tsx
+++ src/app/support-us/page.tsx
@@ -1,11 +1,14 @@
+"use client";
 import Budget from "@/components/support-us/Budget";
 import Hero from "@/components/support-us/Hero";
+import PhaseBudget from "@/components/support-us/PhaseBudget";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-y-[5rem]">
+    <div className="flex flex-col gap-defaultMob lg:gap-default">
       <Hero />
       <Budget />
+      <PhaseBudget />
     </div>
   );
 }
