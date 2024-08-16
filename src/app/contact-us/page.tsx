6fc0ee89b5cf1f897a802987f42526c6fdb15910
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-08-16 15:28:33 -0700

    Add contact us hero

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/contact-us/Hero";
+
+function Page() {
+  return (
+    <div className="flex flex-col gap-defaultMob lg:gap-default px-4 lg:px-12 bg-bg2 bg-no-repeat">
+      <Hero />
+    </div>
+  );
+}
+
+export default Page;
