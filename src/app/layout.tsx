f226a9d70c6b5897a0f2ece8cfa6c778968bd0be
Daniel <57362494+daniel-panhead@users.noreply.github.com>
2024-09-02 10:30:53 -0700

    [Homepage] Add new hero graphic (#30)
    * Add new main page hero graphic
    
    * Add footer
    
    * Fix main page hero
    
    * Fix main page hero image + footer links
    
    * organize navbar + footer
    
    ---------
    
    Co-authored-by: Minh Au <auduongminh2004@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/navigations/Navbar";
+import Footer from "@/components/navigations/Footer";
 
 const inter = Inter({ subsets: ["latin"], fallback: ["sans-serif"] });
 const jakarta = Plus_Jakarta_Sans({
@@ -22,9 +23,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={jakarta.variable}>
-      <body className={inter.className + " pb-8 lg:pb-10"}>
+      <body className={inter.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/navigations/navPages.ts
@@ -0,0 +1,22 @@
+export const links = [
+  {
+    name: "Join Us",
+    href: "/join-us",
+  },
+  {
+    name: "Support Us",
+    href: "/support-us",
+  },
+  {
+    name: "Sponsors",
+    href: "/sponsors",
+  },
+  {
+    name: "Contact Us",
+    href: "/contact-us",
+  },
+  {
+    name: "About Us",
+    href: "/about-us",
+  },
+];
